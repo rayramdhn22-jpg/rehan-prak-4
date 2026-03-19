46211bc8f1a97381460c21ed29faaeeff6258cdd
rayramdhn22-jpg <rayramdhn22@gmail.com>
2026-03-20 05:03:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const toggleBtn = document.getElementById("theme-toggle");
+    const navbar = document.querySelector("nav");
+    const links = document.querySelectorAll("nav ul li a");
+
+    toggleBtn.addEventListener("click", function () {
+        document.body.classList.toggle("light-mode");
+
+        if (document.body.classList.contains("light-mode")) {
+            toggleBtn.textContent = "☀️";
+        } else {
+            toggleBtn.textContent = "🌙";
+        }
+    });
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    });
+
+    links.forEach(function(link) {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            const target = document.querySelector(this.getAttribute("href"));
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+
+});
